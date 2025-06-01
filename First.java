4c0b59e1dc7eea783a32be26370caf84dd984be9
omdhaktode <dhaktodeom@gmail.com>
2025-06-01 11:18:59 +0530

    Creating our first program

---
+++ First.java
@@ -0,0 +1,7 @@
+// our first program 
+
+public class First{
+    public static void main(String[] args){
+        System.out.println("Hello DSA");
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+<p>Creating our first program</p>
